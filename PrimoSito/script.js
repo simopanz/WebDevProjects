b3926e1597521b4fc50abc40b026e81986f94612
simopanz <simonepanzeri.sp@gmail.com>
2025-10-23 12:14:21 +0200

    Add

---
+++ PrimoSito/script.js
@@ -0,0 +1,8 @@
+let bottone=document.getElementById("btn")
+
+bottone.addEventListener("click", sayHello)
+bottone.addEventListener("", sayHello)
+
+function sayHello() {
+    alert("Ciao user! :)")
+}
